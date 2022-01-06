380290be827c3aab410330ea6b04f1fdb42d345a
Alin Apostol <alin.apostol@starbyte.ro>
2022-01-06 15:54:47 +0200

    Added CommanderRepository contract and mock

---
+++ Commander/Data/SpecificCommanderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class SpecificCommanderRepository : ICommanderRepository
+    {
+        public Command GetCommand(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetCommands()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
