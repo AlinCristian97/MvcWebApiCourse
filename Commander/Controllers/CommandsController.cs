024d0280f843205d1378979c9e79165ee832d461
Alin Apostol <alin.apostol@starbyte.ro>
2022-01-06 16:08:17 +0200

    Created CommandsController class

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    // api/commands
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+    }
+}
